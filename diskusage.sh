b3bb92102bc354e323df17bee1b21dd4a37b788f
Joseph Walters <JoeWalters.email@gmail.com>
2016-12-14 09:56:26 -0800

    Delete diskusage.sh
    contents merged with V2

--- diskusage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DISK=/mnt/common
-LOG=/root/usageovertimeV2.csv
-if grep -qs $DISK /proc/mounts; then
-    echo "$(date +%Y%m%d),$(df $DISK | grep $DISK | awk '{print $3}')" >> $LOG
-fi
